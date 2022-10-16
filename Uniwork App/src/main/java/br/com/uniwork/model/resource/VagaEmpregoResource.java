9a4a3673fb0a722aba4a0a5651a97124d96b23c5
Mateus-Cabral <101184627+Mateus-Cabral@users.noreply.github.com>
2022-10-16 15:55:11 -0300

    Resource de Vagas adicionado

---
+++ Uniwork App/src/main/java/br/com/uniwork/model/resource/VagaEmpregoResource.java
@@ -0,0 +1,70 @@
+package br.com.uniwork.model.resource;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import br.com.uniwork.model.bo.VagaEmpregoBO;
+import br.com.uniwork.model.vo.VagaEmpregoVO;
+
+@Path("/vagas")
+public class VagaEmpregoResource {
+
+	VagaEmpregoBO pb = new VagaEmpregoBO();
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<VagaEmpregoVO> buscar() throws SQLException {
+		return pb.listar();
+	}
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public VagaEmpregoVO buscar(@PathParam("id") int id) throws SQLException {
+		return pb.listar(id);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response cadastrar(VagaEmpregoVO vaga, @Context UriInfo uriInfo) throws SQLException {
+
+		// INSERIR NA BASE
+		pb.cadastrar(1,vaga);
+
+		// CONSTRUIR O PATH DE RETORNO
+		UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+		builder.path(Integer.toString(vaga.getEmpresa()));
+
+		// RETORNA O PATH E O STATUS 201
+		return Response.created(builder.build()).build();
+	}
+
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response atualizar(VagaEmpregoVO produtoU, @PathParam("{id}") int id) throws SQLException {
+		pb.atualizar(id,produtoU);
+		return Response.ok().build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public void excluir(@PathParam("id") int id) throws SQLException {
+		pb.deletar(id);
+	}
+
+}
--- Uniwork App/src/main/java/br/com/uniwork/model/vo/VagaEmpregoVO.java
+++ Uniwork App/src/main/java/br/com/uniwork/model/vo/VagaEmpregoVO.java
@@ -6,8 +6,10 @@ package br.com.uniwork.model.vo;
  */
 import java.util.ArrayList;
 
+import javax.xml.bind.annotation.XmlRootElement;
 
 
+@XmlRootElement
 public class VagaEmpregoVO {
 	
 	/**
